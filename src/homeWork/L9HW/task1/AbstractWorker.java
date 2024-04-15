c66ca23bb2cf8fe8fd814e3ba35df83b92a7a30b
MorozovAleksandr <iamaleks.morozov@gmail.com>
2024-04-15 23:48:26 +0300

    task1

--- src/homeWork/L9HW/task1/AbstractWorker.java
+++ src/homeWork/L9HW/task1/AbstractWorker.java
@@ -1,6 +1,6 @@
 package homeWork.L9HW.task1;
 
-public class AbstractWorker implements PrintingInfo {
+public abstract class AbstractWorker implements PrintingInfo {
     @Override
     public void printingJobTitle() {
 
