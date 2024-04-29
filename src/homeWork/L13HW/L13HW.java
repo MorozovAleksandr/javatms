aa972a20121563a2cf0c1eebd451982d10935a81
MorozovAleksandr <iamaleks.morozov@gmail.com>
2024-04-30 00:53:09 +0300

    init

--- src/homeWork/L13HW/L13HW.java
+++ src/homeWork/L13HW/L13HW.java
@@ -4,11 +4,21 @@ import homeWork.L13HW.task1.Credentials;
 
 public class L13HW {
     public static void main(String[] args) {
-        task1();
+        additionalTask();
     }
 
     public static void task1() {
         boolean result = Credentials.checkCredentials("USER", " 1337", "1337");
         System.out.println(result ? "Успех" : "Неуспех");
+    }
+
+    public static void additionalTask() {
+        try {
+            System.out.println(10 / 0);
+        } catch (Exception e) {
+            System.exit(0);
+        } finally {
+            System.out.println("finally");
+        }
     }
 }
